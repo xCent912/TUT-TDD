9d24c5d07af9db9aae8d97818157b2cb881a0d15
xCent912 <vincentiush96722@gmail.com>
2026-05-18 14:40:18 +0700

    feat

--- src/Parking_fee.py
+++ src/Parking_fee.py
@@ -1,3 +1,4 @@
 def calculate_parking_fee(vehicle_type, parking_duration, day_type, is_public_holiday):
-    """Calculate parking fee based on rules."""
+    if vehicle_type == 'motorcycle':
+        return 2
     raise NotImplementedError
