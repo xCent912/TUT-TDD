6b96d23c1391ba621d606d816b4a3a785a1b6d26
xCent912 <vincentiush96722@gmail.com>
2026-05-18 14:48:13 +0700

    feat: implement car and truck base fees

--- src/Parking_fee.py
+++ src/Parking_fee.py
@@ -1,4 +1,3 @@
-def calculate_parking_fee(vehicle_type, parking_duration, day_type, is_public_holiday):
-    if vehicle_type == 'motorcycle':
-        return 2
-    raise NotImplementedError
+BASE_FEES = {'motorcycle': 2, 'car': 5, 'truck': 10}
+def calculate_parking_fee(vehicle_type,parking_duration, day_type,is_public_holiday):
+    return BASE_FEES[vehicle_type]
