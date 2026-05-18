921288fe7caa00f7ec486849938bc0bea96a94f6
xCent912 <vincentiush96722@gmail.com>
2026-05-18 14:42:56 +0700

    test car and truck

--- tests/test_parking_fee.py
+++ tests/test_parking_fee.py
@@ -2,3 +2,9 @@ from src.parking_fee import calculate_parking_fee
 
 def test_motorcycle_base_fee():
     assert calculate_parking_fee('motorcycle', 2, 'weekday', False) == 2
+    
+def test_car_base_fee():
+    assert calculate_parking_fee('car', 2, 'weekday', False) == 5
+    
+def test_truck_base_fee():
+    assert calculate_parking_fee('truck', 2, 'weekday', False) == 10        
